077a0a2314d1a2ba1342dafa9803fb83cdf22d92
dotainion <blair@fireside.solutions>
2021-03-30 03:46:19 -0400

    refactoring

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAXC1nnSRmGv2OeKldUj026Xdy8zaFOQ54",
-    authDomain: "susu-app-f86b6.firebaseapp.com",
-    projectId: "susu-app-f86b6",
-    storageBucket: "susu-app-f86b6.appspot.com",
-    messagingSenderId: "584964030441",
-    appId: "1:584964030441:web:498f07e82ddf47b68a9578",
-    measurementId: "G-KWD1EHQ7MB"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 const app = firebase.initializeApp(FIREBASE_CONFIG);
