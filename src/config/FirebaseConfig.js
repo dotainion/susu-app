2619fee6784f4fea987765ee1961bdd59089d133
dotainion <58013018+dotainion@users.noreply.github.com>
2021-03-30 03:44:54 -0400

    Update FirebaseConfig.js

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAXC1nnSRmGv2OeKldUj026Xdy8zaFOQ54",
-    authDomain: "susu-app-f86b6.firebaseapp.com",
-    projectId: "susu-app-f86b6",
-    storageBucket: "susu-app-f86b6.appspot.com",
-    messagingSenderId: "584964030441",
-    appId: "1:584964030441:web:498f07e82ddf47b68a9578",
-    measurementId: "G-KWD1EHQ7MB"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = firebase.initializeApp(FIREBASE_CONFIG);
